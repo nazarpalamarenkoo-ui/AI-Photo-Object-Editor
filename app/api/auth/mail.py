688a298f105b516a7a9a755bab6a9470dca13733
R3z0l <palamarenkonazar@gmail.com>
2026-04-07 18:06:00 +0300

    feat(email): implement email sending service
    - add SMTP configuration support
    - implement email sending logic
    - integrate MJML templates rendering

---
+++ app/api/auth/mail.py
@@ -0,0 +1,96 @@
+from datetime import datetime, timedelta, timezone
+from json import dumps, loads
+from pathlib import Path
+from fastapi import HTTPException
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
+from pydantic import EmailStr
+from sqlalchemy.ext.asyncio import AsyncSession
+from jose import JWTError, jwt
+
+from app.config.settings import settings
+from app.api.auth.schema import SignUpArgs
+from app.repository.user_repo import UserRepository
+
+mail_config = ConnectionConfig(
+    MAIL_USERNAME = settings.MAIL_USERNAME,
+    MAIL_PASSWORD= settings.MAIL_PASSWORD,
+    MAIL_FROM= settings.MAIL_FROM,
+    MAIL_PORT= settings.MAIL_PORT,
+    MAIL_SERVER= settings.MAIL_SERVER,
+    MAIL_STARTTLS= settings.MAIL_STARTTLS,
+    MAIL_SSL_TLS= settings.MAIL_SSL_TLS,
+    USE_CREDENTIALS= settings.USE_CREDENTIALS,
+    TEMPLATE_FOLDER=Path(__file__).parent / 'email-templates' / 'build'
+)
+
+fm = FastMail(mail_config)
+SECRET_KEY = settings.SECRET_KEY_AUTH
+ALGORITHM = "HS256"
+
+
+async def send_confirmation_email(token: str, signup_args: SignUpArgs):
+    subject = f'Image editor service - email confirmation for user {signup_args.username}'
+    link = f'http://localhost:5173/confirm-email/{token}'
+    message = MessageSchema(
+        subject=subject,
+        recipients=[signup_args.email],
+        template_body={
+            'project_name': 'Image editor service',
+            'username': signup_args.username,
+            'valid_hours': 14,
+            'link': link
+        },
+        subtype=MessageType.html
+    )
+    await fm.send_message(message, template_name='signup_confirmation.html')
+
+
+async def send_reset_password_email(db: AsyncSession, email: EmailStr, token: str):
+    user = await UserRepository(db).get_by_email(email)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    subject = f'Image editor service - password recovery for user {user.username}'
+    link = f'http://localhost:5173/reset-password/{token}'
+    message = MessageSchema(
+        subject=subject,
+        recipients=[email],
+        template_body={
+            'project_name': 'Image editor service',
+            'username': user.username,
+            'valid_hours': 14,
+            'link': link
+        },
+        subtype=MessageType.html
+    )
+    await fm.send_message(message, template_name='reset_password.html')
+
+
+def generate_password_reset_token(email: EmailStr) -> str:
+    expires = datetime.now(timezone.utc) + timedelta(hours=14)
+    return jwt.encode({"exp": expires, "sub": email}, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def validate_password_reset_token(token: str) -> EmailStr:
+    try:
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        return str(decoded_token["sub"])
+    except JWTError:
+        raise HTTPException(status_code=400, detail="Invalid token")
+
+
+def generate_signup_confirmation_token(signup_args: SignUpArgs) -> str:
+    expires = datetime.now(timezone.utc) + timedelta(hours=14)
+    return jwt.encode(
+        {"exp": expires, "sub": dumps(signup_args.model_dump())},
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+
+def validate_signup_confirmation_token(token: str) -> SignUpArgs:
+    try:
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        return SignUpArgs(**loads(decoded_token["sub"]))
+    except JWTError as e:
+        raise HTTPException(status_code=400, detail=str(e))
