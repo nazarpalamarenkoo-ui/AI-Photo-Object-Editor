378d7bf10840a585b6b3b2f2f8852448d62cb5e5
Nazar Palamarenko <nazarpalamarenkoo@gmail.com>
2026-04-30 17:45:51 +0300

    feat(db): add Alembic migration for bbox_id and clean up cache files

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
